5c910126f0bb1786d61b0005ae28106e58ee40fc
IronWindow <91862936+IronWindow@users.noreply.github.com>
2024-02-16 15:27:21 +0800

    Add files via upload

---
+++ classic_model_test/CNN-for-Time-Series-Prediction/core/CNN_modified_model.py
@@ -0,0 +1,156 @@
+import os
+import math
+import numpy as np
+import datetime as dt
+from numpy import newaxis
+from core.utils import Timer
+from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, Flatten
+from keras.models import Sequential, load_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+class Model():
+    """A class for an building and inferencing an lstm model"""
+
+    def __init__(self):
+        self.model = Sequential()
+
+    def load_model(self, filepath):
+        print('[Model] Loading model from file %s' % filepath)
+        self.model = load_model(filepath)
+
+    def build_model(self, configs):
+        timer = Timer()
+        timer.start()
+
+        for layer in configs['model']['layers']:
+            neurons = layer['neurons'] if 'neurons' in layer else None
+            dropout_rate = layer['rate'] if 'rate' in layer else None
+            activation = layer['activation'] if 'activation' in layer else None
+            return_seq = layer['return_seq'] if 'return_seq' in layer else None
+            input_timesteps = layer['input_timesteps'] if 'input_timesteps' in layer else None
+            input_dim = layer['input_dim'] if 'input_dim' in layer else None
+
+            if layer['type'] == 'cnn':
+                if 'input_timesteps' in layer and 'input_dim' in layer:
+                    self.model.add(Conv1D(filters=layer['neurons'],
+                                          kernel_size=layer['kernel_size'],
+                                          activation='relu',
+                                          input_shape=(layer['input_timesteps'], layer['input_dim'])))
+                else:
+                    self.model.add(Conv1D(filters=layer['neurons'],
+                                          kernel_size=layer['kernel_size'],
+                                          activation='relu'))
+                self.model.add(MaxPooling1D(pool_size=2))
+
+            elif layer['type'] == 'dropout':
+                self.model.add(Dropout(layer['rate']))
+
+            elif layer['type'] == 'dense':
+                # 只在最后一个卷积层后添加Flatten层
+                if 'flattened' not in locals():
+                    self.model.add(Flatten())
+                    flattened = True
+                self.model.add(Dense(layer['neurons'], activation=layer['activation']))
+
+
+
+
+        self.model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'])
+
+        print('[Model] Model Compiled')
+        timer.stop()
+
+    def train(self, x, y, epochs, batch_size, save_dir):
+        timer = Timer()
+        timer.start()
+        print('[Model] Training Started')
+        print('[Model] %s epochs, %s batch size' % (epochs, batch_size))
+
+        save_fname = os.path.join(save_dir, '%s-e%s.h5' % (dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(epochs)))
+        callbacks = [
+            EarlyStopping(monitor='val_loss', patience=2),
+            ModelCheckpoint(filepath=save_fname, monitor='val_loss', save_best_only=True)
+        ]
+        self.model.fit(
+            x,
+            y,
+            epochs=epochs,
+            batch_size=batch_size,
+            callbacks=callbacks
+        )
+        self.model.save(save_fname)
+
+        print('[Model] Training Completed. Model saved as %s' % save_fname)
+        timer.stop()
+
+    def train_generator(self, data_gen, epochs, batch_size, steps_per_epoch, save_dir, sentiment_type, model_name, num_csvs):
+        timer = Timer()
+        timer.start()
+        print('[Model] Training Started')
+        print('[Model] %s epochs, %s batch size, %s batches per epoch' % (epochs, batch_size, steps_per_epoch))
+        model_path = f"{model_name}_{sentiment_type}_{num_csvs}.h5"
+        # save_fname = os.path.join(save_dir, '%s-e%s.h5' % (dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(epochs)))
+        save_fname = os.path.join(save_dir, model_path)
+
+        callbacks = [
+            ModelCheckpoint(filepath=save_fname, monitor='loss', save_best_only=True)
+        ]
+        self.model.fit_generator(
+            data_gen,
+            steps_per_epoch=steps_per_epoch,
+            epochs=epochs,
+            callbacks=callbacks,
+            workers=1
+        )
+
+        print('[Model] Training Completed. Model saved as %s' % save_fname)
+        timer.stop()
+
+    def predict_point_by_point(self, data):
+        # Predict each timestep given the last sequence of true data, in effect only predicting 1 step ahead each time
+        print('[Model] Predicting Point-by-Point...')
+        predicted = self.model.predict(data)
+        predicted = np.reshape(predicted, (predicted.size,))
+        return predicted
+
+    def predict_sequences_multiple(self, data, window_size, prediction_len):
+        # Default: window_size， 50, prediction_len，50
+        # Predict sequence of 50 steps before shifting prediction run forward by 50 steps
+        print('[Model] Predicting Sequences Multiple...')
+        prediction_seqs = []
+        for i in range(int(len(data) / prediction_len)):
+            curr_frame = data[i * prediction_len]
+            predicted = []
+            for j in range(prediction_len):
+                predicted.append(self.model.predict(curr_frame[newaxis, :, :])[0, 0])
+                curr_frame = curr_frame[1:]
+                curr_frame = np.insert(curr_frame, [window_size - 2], predicted[-1], axis=0)
+            prediction_seqs.append(predicted)
+        return prediction_seqs
+
+
+    def predict_sequences_multiple_modified(self, data, window_size, prediction_len):
+        # window_size = 50, prediction_len = 3
+        prediction_seqs = []
+        for i in range(0, len(data), prediction_len):
+            curr_frame = data[i]
+            predicted = []
+            for j in range(prediction_len):
+                predicted.append(self.model.predict(curr_frame[newaxis, :, :], verbose=0)[0, 0])
+                curr_frame = curr_frame[1:]
+                curr_frame = np.insert(curr_frame, [window_size - 2], predicted[-1], axis=0)
+                # curr_frame = np.append(curr_frame, predicted[-1])
+            prediction_seqs.append(predicted)
+        return prediction_seqs
+
+    def predict_sequence_full(self, data, window_size):
+      #Shift the window by 1 new prediction each time, re-run predictions on new window
+      print('[Model] Predicting Sequences Full...')
+      curr_frame = data[0]
+      predicted = []
+      for i in range(len(data)):
+        predicted.append(self.model.predict(curr_frame[newaxis,:,:], verbose=0)[0,0])
+        curr_frame = curr_frame[1:]
+        curr_frame = np.insert(curr_frame, [window_size-2], predicted[-1], axis=0)
+      return predicted
