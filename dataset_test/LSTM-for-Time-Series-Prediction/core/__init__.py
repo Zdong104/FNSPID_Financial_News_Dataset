7e77a40c8d782c84fdedeec8036f39132f06d228
Z Dong <77849792+Zdong104@users.noreply.github.com>
2024-02-19 00:37:53 -0500

    Add files via upload

---
+++ dataset_test/LSTM-for-Time-Series-Prediction/core/__init__.py
@@ -0,0 +1,11 @@
+# this module implementation provides an implementation
+# of time series prediction using LSTM. It is provided
+# as is with no warranties or support.
+
+__author__ = "Xinyu Fan"
+__copyright__ = "Xinyu Fan 2024"
+__version__ = "1.0.0"
+# __license__ = "MIT"
+
+import warnings
+warnings.filterwarnings("ignore") # ignore messy numpy warnings
