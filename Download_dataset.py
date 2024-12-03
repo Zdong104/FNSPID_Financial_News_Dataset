c97ac7244f258814602a60f96a0ffc99c7aee012
Z Dong <77849792+Zdong104@users.noreply.github.com>
2024-12-03 10:28:50 -0500

    Update Download_dataset.py

--- Download_dataset.py
+++ Download_dataset.py
@@ -1,6 +1,1 @@
-# You can download the dataset by running this script
-# Remember to pip install datasets before running this script
-from datasets import load_dataset
-
-# Download the dataset and save to the current directory
-dataset = load_dataset("Zihan1004/FNSPID", cache_dir=".")
+# You dont need this 
